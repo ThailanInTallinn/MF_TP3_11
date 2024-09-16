3c3614c5a6eb3f8cc6f8433d5ec8d2e5cca4819b
Thailan Gouvea <thailangm@icloud.com>
2024-09-16 20:16:17 +0300

    Instances product component and sets up images array

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Product from "./components/product/product";
+
+export default function App() {
+  const imgList = [];
+
+  return (
+    <div className="App">
+      <Product />
+    </div>
+  );
+}
