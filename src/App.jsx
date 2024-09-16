7a0f915d85f4f6e90dd47ecb190ae57df5c4e48d
Thailan Gouvea <thailangm@icloud.com>
2024-09-16 20:56:32 +0300

    Starts implementing products image

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import "./App.css";
 import Product from "./components/product/product";
 
 export default function App() {
-  const imgList = [];
+  const imgList = [
+    "https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/embody_chairs/ig_prd_ovw_embody_chairs_01.jpg.rendition.1152.1152.jpg",
+    "https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/embody_chairs/ig_prd_ovw_embody_chairs_02.jpg.rendition.1152.1152.jpg",
+    "https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/embody_chairs/ig_prd_ovw_embody_chairs_03.jpg.rendition.1152.1152.jpg",
+  ];
 
   return (
     <div className="App">
-      <Product />
+      <Product img={[...imgList]} />
     </div>
   );
 }
--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -3,7 +3,7 @@ import styles from "./product.module.css";
 export default function Product({ img, name, price, description, specs }) {
   return (
     <div className={styles.productContainer}>
-      <img src={img} />
+      <img src={img[0]} />
       <div className={styles.infoContainer}>
         <h2>{name}</h2>
         <p>{price}</p>
