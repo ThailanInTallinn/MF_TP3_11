e1ecf698e7fe3d2665b02bdf61a388d6ae9a8964
Thailan Gouvea <thailangm@icloud.com>
2024-09-16 19:53:55 +0300

    Adds product's info

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,9 +1,17 @@
 import styles from "./product.module.css";
 
-export default function Product({ img }) {
+export default function Product({ img, name, price, description, specs }) {
   return (
     <div className={styles.productContainer}>
       <img src={img} />
+      <div className={styles.infoContainer}>
+        <h2>{name}</h2>
+        <p>{price}</p>
+        <h3>Descrição</h3>
+        <p>{description}</p>
+        <h3>Especificações</h3>
+        <p>{specs}</p>
+      </div>
     </div>
   );
 }
